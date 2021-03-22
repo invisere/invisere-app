ca6a8a6b1696d03b5a681bfdcda82a5c80c5dd76
Jordi Mateo <jordi.mateo@udl.cat>
2021-03-22 12:19:09 +0100

    optimize imports and try to keep main class without code comment. Your project allow lambda functions of java 8, so you can use it ^^

--- client/app/src/main/java/dam/invisere/gtidic/udl/cat/invisereapp/utils/EULA.java
+++ client/app/src/main/java/dam/invisere/gtidic/udl/cat/invisereapp/utils/EULA.java
@@ -1,10 +1,8 @@
 package dam.invisere.gtidic.udl.cat.invisereapp.utils;
 
-import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.DialogInterface;
-import android.content.pm.ActivityInfo;
 import android.os.Build;
 import android.os.Bundle;
 import android.text.Html;
@@ -16,7 +14,6 @@ import androidx.annotation.RequiresApi;
 import androidx.fragment.app.DialogFragment;
 
 import dam.invisere.gtidic.udl.cat.invisereapp.R;
-import dam.invisere.gtidic.udl.cat.invisereapp.SignupFragment;
 
 public class EULA extends DialogFragment {
 
@@ -28,7 +25,7 @@ public class EULA extends DialogFragment {
      * so next time this will not show, until next upgrade.
      */
 
-    private CheckBox checkBox;
+    private final CheckBox checkBox;
 
     public EULA(CheckBox checkBox) {
         this.checkBox = checkBox;
@@ -42,34 +39,13 @@ public class EULA extends DialogFragment {
         return new AlertDialog.Builder(requireContext())
                 .setMessage(Html.fromHtml(getString(R.string.eula_string), Build.VERSION.SDK_INT))
                 .setPositiveButton(R.string.accept,
-                        new Dialog.OnClickListener() {
-                            @Override
-                            public void onClick(DialogInterface dialogInterface, int i) {
-                                // Close dialog
-                                dialogInterface.dismiss();
-
-                                // Enable orientation changes based on
-                                // device's sensor
-//                                mContext.setRequestedOrientation(
-//                                        ActivityInfo.SCREEN_ORIENTATION_SENSOR);
-
-                                checkBox.setChecked(true);
-
-                            }
-
+                        (dialogInterface, i) -> {
+                            dialogInterface.dismiss();
+                            checkBox.setChecked(true);
 
                         })
                 .setNegativeButton(android.R.string.cancel,
-                new Dialog.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        // Close the activity as they have declined
-                        // the EULA
-//                        mContext.setRequestedOrientation(
-//                                ActivityInfo.SCREEN_ORIENTATION_SENSOR);
-                        checkBox.setChecked(false);
-                    }
-                })
+                        (dialog, which) -> checkBox.setChecked(false))
                 .create();
     }
     public static String TAG = "EULAConfirmationDialog";
