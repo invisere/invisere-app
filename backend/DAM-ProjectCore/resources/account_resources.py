a88235e70dba54a2cbc4c105966b11d392106373
Monica <monica.casin8@gmail.com>
2021-04-18 18:13:51 +0200

    update template

--- backend/DAM-ProjectCore/resources/account_resources.py
+++ backend/DAM-ProjectCore/resources/account_resources.py
@@ -121,204 +121,172 @@ class ResourceAccountRecovery(DAMCoreResource):
 
             html = """\
             <!DOCTYPE html>
-            <html>
-            <head>
+			<html lang="en-US">
+			  <head>
+			    <meta content="text/html; charset=utf-8" http-equiv="Content-Type" />
+			    <title>Reset Password Email Template</title>
+			    <meta name="description" content="Reset Password Email Template." />
+			    <style type="text/css">
+			      a:hover {
+			        text-decoration: underline !important;
+			      }
+			    </style>
+			  </head>
 
-            <meta charset="utf-8">
-            <meta http-equiv="x-ua-compatible" content="ie=edge">
-            <title>Password Reset</title>
-            <meta name="viewport" content="width=device-width, initial-scale=1">
-            <style type="text/css">
-            /**
-            * Google webfonts. Recommended to include the .woff version for cross-client compatibility.
-            */
-            @media screen {
-                @font-face {
-                font-family: 'Source Sans Pro';
-                font-style: normal;
-                font-weight: 400;
-                src: local('Source Sans Pro Regular'), local('SourceSansPro-Regular'), url(https://fonts.gstatic.com/s/sourcesanspro/v10/ODelI1aHBYDBqgeIAH2zlBM0YzuT7MdOe03otPbuUS0.woff) format('woff');
-                }
+			  <body
+			    marginheight="0"
+			    topmargin="0"
+			    marginwidth="0"
+			    style="margin: 0px; background-color: #f2f3f8"
+			    leftmargin="0"
+			  >
+			    <!--100% body table-->
+			    <table
+			      cellspacing="0"
+			      border="0"
+			      cellpadding="0"
+			      width="100%"
+			      bgcolor="#f2f3f8"
+			      style="
+			        @import url(https://fonts.googleapis.com/css?family=Rubik:300,400,500,700|Open+Sans:300,400,600,700);
+			        font-family: 'Open Sans', sans-serif;
+			      "
+			    >
+			      <tr>
+			        <td>
+			          <table
+			            style="background-color: #f2f3f8; max-width: 670px; margin: 0 auto"
+			            width="100%"
+			            border="0"
+			            align="center"
+			            cellpadding="0"
+			            cellspacing="0"
+			          >
+			            <tr>
+			              <td style="height: 80px">&nbsp;</td>
+			            </tr>
+			            <tr>
+			              <td style="text-align: center">
+			                <a href="https://invisere.tech" title="logo" target="_blank">
+			                  <img
+			                    width="100"
+			                    src="cid:0"
+			                    title="logo"
+			                    alt="logo"
+			                  />
+			                </a>
+			              </td>
+			            </tr>
+			            <tr>
+			              <td style="height: 20px">&nbsp;</td>
+			            </tr>
+			            <tr>
+			              <td>
+			                <table
+			                  width="95%"
+			                  border="0"
+			                  align="center"
+			                  cellpadding="0"
+			                  cellspacing="0"
+			                  style="
+			                    max-width: 670px;
+			                    background: #fff;
+			                    border-radius: 3px;
+			                    text-align: center;
+			                    -webkit-box-shadow: 0 6px 18px 0 rgba(0, 0, 0, 0.06);
+			                    -moz-box-shadow: 0 6px 18px 0 rgba(0, 0, 0, 0.06);
+			                    box-shadow: 0 6px 18px 0 rgba(0, 0, 0, 0.06);
+			                  "
+			                >
+			                  <tr>
+			                    <td style="height: 40px">&nbsp;</td>
+			                  </tr>
+			                  <tr>
+			                    <td style="padding: 0 35px">
+			                      <h1
+			                        style="
+			                          color: #1e1e2d;
+			                          font-weight: 500;
+			                          margin: 0;
+			                          font-size: 32px;
+			                          font-family: 'Rubik', sans-serif;
+			                        "
+			                      >
+			                        You have requested to reset your password
+			                      </h1>
+			                      <span
+			                        style="
+			                          display: inline-block;
+			                          vertical-align: middle;
+			                          margin: 29px 0 26px;
+			                          border-bottom: 1px solid #cecece;
+			                          width: 100px;
+			                        "
+			                      ></span>
+			                      <p
+			                        style="
+			                          color: #455056;
+			                          font-size: 15px;
+			                          line-height: 24px;
+			                          margin: 0;
+			                        "
+			                      >
+			                        We cannot simply send you your old password. <br />A
+			                        unique code to reset your password has been generated
+			                        for you. <br />To reset your password, enter the code in
+			                        the app.
+			                      </p>
+			                      <a
+			                        style="
+			                          background: #008aff;
+			                          text-decoration: none !important;
+			                          font-weight: bold;
+			                          margin-top: 35px;
+			                          color: #fff;
+			                          text-transform: uppercase;
+			                          font-size: 24px;
+			                          padding: 10px 24px;
+			                          display: inline-block;
+			                          border-radius: 10px;
+			                        "
+			                        >{{code}}</a
+			                      >
+			                    </td>
+			                  </tr>
+			                  <tr>
+			                    <td style="height: 40px">&nbsp;</td>
+			                  </tr>
+			                </table>
+			              </td>
+			            </tr>
 
-                @font-face {
-                font-family: 'Source Sans Pro';
-                font-style: normal;
-                font-weight: 700;
-                src: local('Source Sans Pro Bold'), local('SourceSansPro-Bold'), url(https://fonts.gstatic.com/s/sourcesanspro/v10/toadOcfmlt9b38dHJxOBGFkQc6VGVFSmCnC_l7QZG60.woff) format('woff');
-                }
-            }
-
-            /**
-            * Avoid browser level font resizing.
-            * 1. Windows Mobile
-            * 2. iOS / OSX
-            */
-            body,
-            table,
-            td,
-            a {
-                -ms-text-size-adjust: 100%; /* 1 */
-                -webkit-text-size-adjust: 100%; /* 2 */
-            }
-
-            /**
-            * Remove extra space added to tables and cells in Outlook.
-            */
-            table,
-            td {
-                mso-table-rspace: 0pt;
-                mso-table-lspace: 0pt;
-            }
-
-            /**
-            * Better fluid images in Internet Explorer.
-            */
-            img {
-                -ms-interpolation-mode: bicubic;
-            }
-
-            /**
-            * Remove blue links for iOS devices.
-            */
-            a[x-apple-data-detectors] {
-                font-family: inherit !important;
-                font-size: inherit !important;
-                font-weight: inherit !important;
-                line-height: inherit !important;
-                color: inherit !important;
-                text-decoration: none !important;
-            }
-
-            /**
-            * Fix centering issues in Android 4.4.
-            */
-            div[style*="margin: 16px 0;"] {
-                margin: 0 !important;
-            }
-
-            body {
-                width: 100% !important;
-                height: 100% !important;
-                padding: 0 !important;
-                margin: 0 !important;
-            }
-
-            /**
-            * Collapse table borders to avoid space between cells.
-            */
-            table {
-                border-collapse: collapse !important;
-            }
-
-            a {
-                color: #1a82e2;
-            }
-
-            img {
-                height: auto;
-                line-height: 100%;
-                text-decoration: none;
-                border: 0;
-                outline: none;
-            }
-            </style>
-
-            </head>
-            <body style="background-color: #e9ecef;">
-
-            <!-- start preheader -->
-            <div class="preheader" style="display: none; max-width: 0; max-height: 0; overflow: hidden; font-size: 1px; line-height: 1px; color: #fff; opacity: 0;">
-                A preheader is the short summary text that follows the subject line when an email is viewed in the inbox.
-            </div>
-            <!-- end preheader -->
-
-            <!-- start body -->
-            <table border="0" cellpadding="0" cellspacing="0" width="100%">
-
-                <!-- start logo -->
-                <tr>
-                <td align="center" bgcolor="#e9ecef">
-                    <!--[if (gte mso 9)|(IE)]>
-                    <table align="center" border="0" cellpadding="0" cellspacing="0" width="600">
-                    <tr>
-                    <td align="center" valign="top" width="600">
-                    <![endif]-->
-                    <table border="0" cellpadding="0" cellspacing="0" width="100%" style="max-width: 600px;">
-                    <tr>
-                        <td align="center" valign="top" style="padding: 36px 24px;">
-                        <a href="https://github.com/Invisere/invisere-app" target="_blank" style="display: inline-block;">
-                            <img src="cid:0" alt="Logo" border="0" width="48" style="display: block; width: 48px; max-width: 48px; min-width: 48px;">
-                        </a>
-                        </td>
-                    </tr>
-                    </table>
-                    <!--[if (gte mso 9)|(IE)]>
-                    </td>
-                    </tr>
-                    </table>
-                    <![endif]-->
-                </td>
-                </tr>
-                <!-- end logo -->
-
-                <!-- start hero -->
-                <tr>
-                <td align="center" bgcolor="#e9ecef">
-                    <!--[if (gte mso 9)|(IE)]>
-                    <table align="center" border="0" cellpadding="0" cellspacing="0" width="600">
-                    <tr>
-                    <td align="center" valign="top" width="600">
-                    <![endif]-->
-                    <table border="0" cellpadding="0" cellspacing="0" width="100%" style="max-width: 600px;">
-                    <tr>
-                        <td align="left" bgcolor="#ffffff" style="padding: 36px 24px 0; font-family: 'Source Sans Pro', Helvetica, Arial, sans-serif; border-top: 3px solid #d4dadf;">
-                        <h1 style="margin: 0; font-size: 32px; font-weight: 700; letter-spacing: -1px; line-height: 48px;">Reset Your Password</h1>
-                        </td>
-                    </tr>
-                    </table>
-                    <!--[if (gte mso 9)|(IE)]>
-                    </td>
-                    </tr>
-                    </table>
-                    <![endif]-->
-                </td>
-                </tr>
-                <!-- end hero -->
-
-                <!-- start copy block -->
-                <tr>
-                <td align="center" bgcolor="#e9ecef">
-                    <!--[if (gte mso 9)|(IE)]>
-                    <table align="center" border="0" cellpadding="0" cellspacing="0" width="600">
-                    <tr>
-                    <td align="center" valign="top" width="600">
-                    <![endif]-->
-                    <table border="0" cellpadding="0" cellspacing="0" width="100%" style="max-width: 600px;">
-
-                    <!-- start copy -->
-                    <tr>
-                        <td align="left" bgcolor="#ffffff" style="padding: 24px; font-family: 'Source Sans Pro', Helvetica, Arial, sans-serif; font-size: 16px; line-height: 24px;">
-                        <p style="margin: 0;">Please, copy the code below to the recovery password screen in your application and then set the new password.</p>
-                        <p style="margin: 0;">Code: {{code}}</p>
-                        </td>
-                    </tr>
-                    <!-- end copy -->
-
-                    
-                    </table>
-                    <!--[if (gte mso 9)|(IE)]>
-                    </td>
-                    </tr>
-                    </table>
-                    <![endif]-->
-                </td>
-                </tr>
-                <!-- end copy block -->
-
-            </table>
-            <!-- end body -->
-
-            </body>
+			            <tr>
+			              <td style="height: 20px">&nbsp;</td>
+			            </tr>
+			            <tr>
+			              <td style="text-align: center">
+			                <p
+			                  style="
+			                    font-size: 14px;
+			                    color: rgba(69, 80, 86, 0.7411764705882353);
+			                    line-height: 18px;
+			                    margin: 0 0 0;
+			                  "
+			                >
+			                  &copy; <strong>invisere.tech</strong>
+			                </p>
+			              </td>
+			            </tr>
+			            <tr>
+			              <td style="height: 80px">&nbsp;</td>
+			            </tr>
+			          </table>
+			        </td>
+			      </tr>
+			    </table>
+			    <!--/100% body table-->
+			  </body>
+			</html>
 
             """
 
