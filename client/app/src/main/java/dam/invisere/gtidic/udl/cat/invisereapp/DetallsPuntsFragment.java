38e667792660311bc00c4d0d063d9fee0b2af516
Cristian Oprea <72124673+xCR055@users.noreply.github.com>
2021-05-24 17:18:18 +0200

    Error en el get_routes del DetallsPuntsFragment

--- client/app/src/main/java/dam/invisere/gtidic/udl/cat/invisereapp/DetallsPuntsFragment.java
+++ client/app/src/main/java/dam/invisere/gtidic/udl/cat/invisereapp/DetallsPuntsFragment.java
@@ -5,21 +5,40 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
 
+import dam.invisere.gtidic.udl.cat.invisereapp.databinding.FragmentDetallsPuntsBinding;
+import dam.invisere.gtidic.udl.cat.invisereapp.models.Place;
+import dam.invisere.gtidic.udl.cat.invisereapp.repo.AccountRepo;
+import dam.invisere.gtidic.udl.cat.invisereapp.utils.Utils;
+
 public class DetallsPuntsFragment extends Fragment {
+
+    private AccountRepo accountRepo;
 
     public DetallsPuntsFragment() {}
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        accountRepo = new AccountRepo();
+        accountRepo.get_routes(Utils.getToken());
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_detalls_punts, container, false);
+        FragmentDetallsPuntsBinding binding = DataBindingUtil.inflate(inflater, R.layout.fragment_detalls_punts, container, false);
+        View view = binding.getRoot();
+
+        accountRepo.getRoutesList().observe(getViewLifecycleOwner(), routes -> {
+            Place[] places = accountRepo.getRoutesList().getValue().get(0).getPoints();
+            binding.setPlace(places[0]);
+        });
+
+
+        return view;
     }
 }
