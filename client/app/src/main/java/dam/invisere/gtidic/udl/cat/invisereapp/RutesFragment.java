e0e8a3b5b739e235c6ec9974ca4ff1ea58fa6d6e
Martí Aguilera <mrtxaver@gmail.com>
2021-05-24 16:53:19 +0200

    FragmentRoutes

--- client/app/src/main/java/dam/invisere/gtidic/udl/cat/invisereapp/RutesFragment.java
+++ client/app/src/main/java/dam/invisere/gtidic/udl/cat/invisereapp/RutesFragment.java
@@ -6,16 +6,22 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.LifecycleOwner;
+import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import dam.invisere.gtidic.udl.cat.invisereapp.adapter.RouteAdapter;
 import dam.invisere.gtidic.udl.cat.invisereapp.adapter.RouteDiffCallback;
+import dam.invisere.gtidic.udl.cat.invisereapp.databinding.FragmentLoginBinding;
+import dam.invisere.gtidic.udl.cat.invisereapp.databinding.FragmentSignupBinding;
 import dam.invisere.gtidic.udl.cat.invisereapp.preferences.Preferences;
 import dam.invisere.gtidic.udl.cat.invisereapp.repo.AccountRepo;
 import dam.invisere.gtidic.udl.cat.invisereapp.viewmodels.ListRoutesViewModel;
+import dam.invisere.gtidic.udl.cat.invisereapp.viewmodels.LoginViewModel;
+import dam.invisere.gtidic.udl.cat.invisereapp.viewmodels.SignUpViewModel;
 
 import static android.content.ContentValues.TAG;
 
@@ -26,8 +32,8 @@ public class RutesFragment extends Fragment {
     public RouteAdapter routeAdapter;
 
     // TODO: Rename and change types and number of parameters
-    public static RoutesListFragment newInstance() {
-        RoutesListFragment fragment = new RoutesListFragment();
+    public static RutesFragment newInstance() {
+        RutesFragment fragment = new RutesFragment();
         Bundle args = new Bundle();
         fragment.setArguments(args);
         return fragment;
@@ -41,19 +47,19 @@ public class RutesFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_rutes, container, false);
 
+        listRoutesViewModel = new ListRoutesViewModel();
         routeAdapter = new RouteAdapter(new RouteDiffCallback());
-        recyclerView = recyclerView.findViewById(R.id.activityMainRcyMain2);
+        recyclerView = view.findViewById(R.id.activityMainRcyMain2);
         recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
         recyclerView.setAdapter(routeAdapter);
 
         initView();
-        return inflater.inflate(R.layout.fragment_rutes, container, false);
+        return view;
     }
 
     public void initView(){
-        listRoutesViewModel = new ListRoutesViewModel();
-
         listRoutesViewModel.getRoutes();
 
         listRoutesViewModel.returnRoutes().observe((LifecycleOwner) getContext(), routes -> {
