d6df6dafc2ed7ff1547fb9c2f3a07f9b21c78793
Martí Aguilera <mrtxaver@gmail.com>
2021-05-24 16:19:15 +0200

    Merge

--- client/app/src/main/java/dam/invisere/gtidic/udl/cat/invisereapp/RutesFragment.java
+++ client/app/src/main/java/dam/invisere/gtidic/udl/cat/invisereapp/RutesFragment.java
@@ -7,38 +7,58 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.LifecycleOwner;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import dam.invisere.gtidic.udl.cat.invisereapp.adapter.RouteAdapter;
+import dam.invisere.gtidic.udl.cat.invisereapp.adapter.RouteDiffCallback;
 import dam.invisere.gtidic.udl.cat.invisereapp.preferences.Preferences;
 import dam.invisere.gtidic.udl.cat.invisereapp.repo.AccountRepo;
+import dam.invisere.gtidic.udl.cat.invisereapp.viewmodels.ListRoutesViewModel;
 
 import static android.content.ContentValues.TAG;
 
 public class RutesFragment extends Fragment {
 
-    private AccountRepo accountRepo;
+    public RecyclerView recyclerView;
+    public ListRoutesViewModel listRoutesViewModel;
+    public RouteAdapter routeAdapter;
 
-    public RutesFragment() {}
+    // TODO: Rename and change types and number of parameters
+    public static RoutesListFragment newInstance() {
+        RoutesListFragment fragment = new RoutesListFragment();
+        Bundle args = new Bundle();
+        fragment.setArguments(args);
+        return fragment;
+    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        accountRepo = new AccountRepo();
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+
+        routeAdapter = new RouteAdapter(new RouteDiffCallback());
+        recyclerView = recyclerView.findViewById(R.id.activityMainRcyMain2);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        recyclerView.setAdapter(routeAdapter);
+
+        initView();
         return inflater.inflate(R.layout.fragment_rutes, container, false);
     }
 
-    public void logout(View view) {
-        String token = Preferences.providePreferences().getString("token","");
-        accountRepo.deleteTokenUser(token);
-        Log.d(TAG, "Logout() -> he rebut el token: " + token);
+    public void initView(){
+        listRoutesViewModel = new ListRoutesViewModel();
 
-        Preferences.providePreferences().edit().clear().apply();
+        listRoutesViewModel.getRoutes();
 
-        getActivity().finish();
+        listRoutesViewModel.returnRoutes().observe((LifecycleOwner) getContext(), routes -> {
+            routeAdapter.submitList(routes);
+            //listRoutesViewModel.getRoutes();
+        });
     }
 }
